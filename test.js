cd76195e57c68b1f5eac0c984623655747a3ac43
c9u11 <tjrdud6412@naver.com>
2022-09-20 23:13:01 +0900

    ✅ Arch Check
    + process.env.ARCH
    + process.env.arch

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const os = require("os");
 
-console.log(os.arch());
+console.log("os.arch() : " + os.arch());
+console.log("process.env.ARCH : " + process.env.ARCH);
+console.log("process.arch : " + process.arch);

f0fecdc0882a1b53c1e44fa74fdf1e1681319382
c9u11 <tjrdud6412@naver.com>
2022-09-21 22:59:02 +0900

    ✅ Arch Check
    + os.version()
    + iohook's arch

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 const os = require("os");
 
+console.log("os.version() : " + os.version());
 console.log("os.arch() : " + os.arch());
 console.log("process.env.ARCH : " + process.env.ARCH);
 console.log("process.arch : " + process.arch);
+
+let arch = process.env.ARCH
+  ? process.env.ARCH.replace("i686", "ia32").replace("x86_64", "x64")
+  : process.arch;
+
+console.log(arch);

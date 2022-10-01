55c5a9170e22d924f47693d23d75e5ec8f2f0cec
c9u11 <tjrdud6412@naver.com>
2022-10-01 12:12:01 +0900

    ✅ robotJS Check
    + add test : move the mouse across the screen as a sine wave.

---
+++ test/robotJs.js
@@ -0,0 +1,15 @@
+// Move the mouse across the screen as a sine wave.
+var robot = require("robotjs");
+
+// Speed up the mouse.
+robot.setMouseDelay(2);
+
+var twoPI = Math.PI * 2.0;
+var screenSize = robot.getScreenSize();
+var height = screenSize.height / 2 - 10;
+var width = screenSize.width;
+
+for (var x = 0; x < width; x++) {
+  y = height * Math.sin((twoPI * x) / width) + height;
+  robot.moveMouse(x, y);
+}

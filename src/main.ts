e3e44766aa984617a678548af867481b3d82979a
c9u11 <tjrdud6412@naver.com>
2022-10-04 23:27:48 +0900

    ✨ Background App Setting
    - Tray
       - Menu
    - Setting Window

--- src/main.ts
+++ src/main.ts
@@ -4,46 +4,73 @@ import {
   globalShortcut,
   clipboard,
   ipcMain,
+  Tray,
+  nativeImage,
+  Menu,
 } from "electron";
 import * as path from "path";
 import { keyTap, typeString } from "robotjs";
 import { convertEngToKor } from "./util/convertEngtoKor";
 
-function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-    width: 800,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  mainWindow.webContents.on("did-finish-load", () => {
-    // onWebcontentsValue 이벤트 송신
-    mainWindow.webContents.send("onWebcontentsValue", "on load...");
-  });
+interface AppMainInterface {
+  settingWindow?: BrowserWindow;
+  tray?: Tray;
+  icons?: BrowserWindow;
 }
+
+const appMain: AppMainInterface = {};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", () => {
-  createWindow();
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  appMain.settingWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    center: true,
+    minimizable: false,
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      webSecurity: true,
+      sandbox: true,
+      contextIsolation: false,
+    },
   });
+  const menu = Menu.buildFromTemplate([
+    {
+      label: "KoEn 활성화",
+      type: "checkbox",
+      click: (item, window, event) => {
+        console.log("활성화 버튼 클릭");
+      },
+    },
+    { type: "separator" },
+    {
+      label: "환경설정...",
+      click: (item, window, event) => {
+        appMain.settingWindow.show();
+      },
+    },
+    { role: "quit", label: "KoEn 종료" },
+  ]);
+  appMain.settingWindow.loadFile(path.join(__dirname, "../index.html"));
+  appMain.settingWindow.webContents.on("did-finish-load", () => {
+    // onWebcontentsValue 이벤트 송신
+    appMain.settingWindow.webContents.send("onWebcontentsValue", "on load...");
+  });
+  appMain.settingWindow.on("close", (ev: Electron.Event) => {
+    appMain.settingWindow.hide();
+    ev.preventDefault();
+  });
+  appMain.tray = new Tray(nativeImage.createEmpty());
+  appMain.tray.setToolTip("KoEn");
+  appMain.tray.setContextMenu(menu);
+  // app.on("activate", function () {
+  //   // On macOS it's common to re-create a window in the app when the
+  //   // dock icon is clicked and there are no other windows open.
+  //   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  // });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common

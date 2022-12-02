d92dd0805209cc51908623011e8d513a51b61989
c9u11 <tjrdud6412@naver.com>
2022-12-02 22:23:52 +0900

    ✅ Testing convertKortoEng
    - 분리된 자음 모음을 영어로 변환하는 로직 테스트

--- test/convertKortoEng.js
+++ test/convertKortoEng.js
@@ -9,26 +9,85 @@ const convertKorToEng = (text) => {
     'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ',
     'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ',
     'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
-
+  const toEng = {
+    'ㄱ': 'r',
+    'ㄲ': 'R',
+    'ㄴ': 's',
+    'ㄷ': 'e',
+    'ㄸ': 'E',
+    'ㄹ': 'f',
+    'ㅁ': 'a',
+    'ㅂ': 'q',
+    'ㅃ': 'Q',
+    'ㅅ': 't',
+    'ㅆ': 'T',
+    'ㅇ': 'd',
+    'ㅈ': 'w',
+    'ㅉ': 'W',
+    'ㅊ': 'c',
+    'ㅋ': 'z',
+    'ㅌ': 'x',
+    'ㅍ': 'v',
+    'ㅎ': 'g',
+    'ㅏ': 'k',
+    'ㅐ': 'o',
+    'ㅑ': 'i',
+    'ㅒ': 'O',
+    'ㅓ': 'j',
+    'ㅔ': 'p',
+    'ㅕ': 'u',
+    'ㅖ': 'P',
+    'ㅗ': 'h',
+    'ㅘ': 'hk',
+    'ㅙ': 'ho',
+    'ㅚ': 'hl',
+    'ㅛ': 'y',
+    'ㅜ': 'n',
+    'ㅝ': 'nj',
+    'ㅞ': 'np',
+    'ㅟ': 'nl',
+    'ㅠ': 'b',
+    'ㅡ': 'm',
+    'ㅢ': 'ml',
+    'ㅣ': 'l',
+    'ㄳ': 'rt',
+    'ㄵ': 'sw',
+    'ㄶ': 'sg',
+    'ㄺ': 'sr',
+    'ㄻ': 'fa',
+    'ㄼ': 'fq',
+    'ㄽ': 'ft',
+    'ㄾ': 'fx',
+    'ㄿ': 'fv',
+    'ㅀ': 'fg',
+    'ㅄ': 'qt'
+  }
   const ga = 44032;
   const result = [];
   for (let char of text) {
+    if (toEng[char]) {
+      result.push(toEng[char]);
+      continue;
+    }
 
-    let uni = char.charCodeAt(0);
-
-    uni = uni - ga;
+    let uni = char.charCodeAt(0) - ga;
 
     let choIdx = parseInt(uni / 588);
     let jungIdx = parseInt((uni - (choIdx * 588)) / 28);
     let jongIdx = parseInt(uni % 28);
-    result.push({
-      cho: CHO[choIdx],
-      jung: JUNG[jungIdx],
-      jong: JONG[jongIdx]
-    })
+    // result.push({
+    //   cho: CHO[choIdx],
+    //   jung: JUNG[jungIdx],
+    //   jong: JONG[jongIdx]
+    // })
+    const cho = toEng[CHO[choIdx]];
+    const jung = toEng[JUNG[jungIdx]];
+    const jong = toEng[JONG[jongIdx]];
+    result.push(cho, jung);
+    if (jong) result.push(jong);
   }
 
   return result;
 };
 
-console.log(convertKorToEng("가나다라다앍가슴살"));
+console.log(convertKorToEng("ㄱ나다라다앍가슴살"));
